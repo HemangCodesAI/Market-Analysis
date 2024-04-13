ef7d03fd57f0498123e76c135392afe637474ab7
Hemang Joshi <Hemanghades@gmail.com>
2024-04-13 12:20:24 +0530

    try splitting

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ import pandas as pd
 import requests
 # capture_data([['Jhu', 'Doe', 'john.doe@example.com']])
 app = Flask(__name__)
-
+a=['Population Growth since 2000','Poverty Rate','College Graduates','Crime Index','Median Household Income','Median Household Income Growth since 2000','Landlord Friendly','Median Household Value','Median Household Value Growth since 2000','Owner Occupied Housing Unit Rate','Median Gross Rent','Median Gross Rent vs. Median HH Income','Max Possible Rent','Liveability Score','Vacancy Rate','Job Growth','Unemployment Rate'] 
+ekpi = pd.DataFrame( columns = ['KPI', 'Value', 'Comment'])
+ekpi['KPI'] = a
+ekpi['Value'] = ['']*17
+ekpi['Comment'] = ['']*17
+c=['Studio', '1-BD', '2-BD', '3-BD', '4-BD']
+erent = pd.DataFrame( columns = ['Type', 'min', 'max'])
+erent['Type'] = c
+erent['min'] = ['']*5
+erent['max'] = ['']*5
 @app.route('/', methods=['GET'])
 def index():
-    a=['Population Growth since 2000','Poverty Rate','College Graduates','Crime Index','Median Household Income','Median Household Income Growth since 2000','Landlord Friendly','Median Household Value','Median Household Value Growth since 2000','Owner Occupied Housing Unit Rate','Median Gross Rent','Median Gross Rent vs. Median HH Income','Max Possible Rent','Liveability Score','Vacancy Rate','Job Growth','Unemployment Rate'] 
-    ekpi = pd.DataFrame( columns = ['KPI', 'Value', 'Comment'])
-    ekpi['KPI'] = a
-    ekpi['Value'] = ['']*17
-    ekpi['Comment'] = ['']*17
-    c=['Studio', '1-BD', '2-BD', '3-BD', '4-BD']
-    erent = pd.DataFrame( columns = ['Type', 'min', 'max'])
-    erent['Type'] = c
-    erent['min'] = ['']*5
-    erent['max'] = ['']*5
+    
     return render_template('1.html', result=False, ekpi=ekpi, erent=erent)
     # return render_template('1.html', result=False)
 
@@ -26,14 +26,14 @@ def submit():
     name = request.form.get('Name')
     phone_number = request.form.get('phoneNumber')
     zipcode = request.form.get('zipcode')
-    user_data=[email,name,phone_number,zipcode]
-    capture_data([user_data])
+    # user_data=[email,name,phone_number,zipcode]
+    # capture_data([user_data])
     info=get_zip_data(zipcode)
     print("0")
     if zipcode and info:
-        rents=get_rent(info)
+        # rents=get_rent(info)
         print("1")
-        KPIs= get_data(info,rents)
+        KPIs= get_data(info,erent)
         print("2")
         url=f'''https://datausa.io/profile/geo/{info.major_city.lower().replace(" ","-").replace("-national","")}-{info.state.lower()}/economy/employment_by_industries?viz=true'''
         url1=f'''https://datausa.io/profile/geo/{info.major_city.lower().replace(" ","-").replace("-national","")}-{info.state.lower()}/education/degrees?viz=true'''
@@ -42,7 +42,7 @@ def submit():
         else:
             jd=False
         print("3")
-        return render_template('1.html', result=True, zipcode=zipcode, KPIs=KPIs, rents=rents, jd=jd, Email=email, Name=name, phoneNumber=phone_number)
+        return render_template('1.html', result=True, zipcode=zipcode, KPIs=KPIs, rents=erent, jd=jd, Email=email, Name=name, phoneNumber=phone_number)
     else:
         return "Please enter a valid zip code!", 400
 
