29a6bc6ae4a3e21b9bafcac9bf01814db0200750
Hemang Joshi <Hemanghades@gmail.com>
2024-04-18 17:45:22 +0530

    kuch bhi

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def submit():
 #     bed_fil = request.form.get('selected_option')
 #     rents=get_rent(info,bed_fil)
 #     return render_template('1.html',more_info=True, result=True, zipcode=user_data[3], KPIs=pd.read_json(session.get('df_json')), rents=rents, jd=user_data[6], Email=user_data[0], Name=user_data[1], phoneNumber=user_data[2])
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
